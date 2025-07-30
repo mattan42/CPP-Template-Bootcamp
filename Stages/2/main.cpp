257735a851ba287384e81bcead307cb4b7653d6c
Mattan <mattanharnoy@gmail.com>
2025-07-30 16:59:39 +0300

    Finished writing the main for stage 2

--- Stages/2/main.cpp
+++ Stages/2/main.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <type_traits>
+#include <functional>
 
 #include "solution.hpp"
 
@@ -10,6 +11,27 @@ TEST(max, accept_integral_types)
     EXPECT_EQ(max<const int>(1, 2), 2);
     EXPECT_EQ(max<long>(1, 2), 2);
     EXPECT_EQ(max<long long>(1, 2), 2);
+
+    static_assert(!std::is_invocable_v<decltype(max<bool>)>, "Can't instantiate max with T=bool.");
+    EXPECT_FALSE(std::is_invocable_v<decltype(max<bool>)>);
+
+    static_assert(!std::is_invocable_v<decltype(max<float>)>, "Can't instantiate max with T=float.");
+    EXPECT_FALSE(std::is_invocable_v<decltype(max<float>)>);
 }
 
-// TODO: Add a helper template to check that the max function tempalte doesn't compile on certain scenarios.
+TEST(max, doesnt_accept_bool_type)
+{
+    static_assert(!std::is_invocable_v<decltype(max<bool>)>, "Can't instantiate max with T=bool.");
+    EXPECT_FALSE(std::is_invocable_v<decltype(max<bool>)>);
+}
+
+TEST(max, doesnt_accept_non_integral_types)
+{
+    static_assert(!std::is_invocable_v<decltype(max<float>)>, "Can't instantiate max with T=float.");
+    EXPECT_FALSE(std::is_invocable_v<decltype(max<float>)>);
+
+    struct Person {};
+    
+    static_assert(!std::is_invocable_v<decltype(max<Person>)>, "Can't instantiate max with T=Person.");
+    EXPECT_FALSE(std::is_invocable_v<decltype(max<Person>)>);
+}
