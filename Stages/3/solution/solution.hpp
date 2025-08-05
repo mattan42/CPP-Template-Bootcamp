92cea9b17e5c7e1fb56cf8a891addbce972b3b36
Mattan <mattanharnoy@gmail.com>
2025-08-06 00:12:23 +0300

    Added the include humans.hpp to the solution.hpp

---
+++ Stages/3/solution/solution.hpp
@@ -0,0 +1,32 @@
+#ifndef SOLUTION_HPP
+#define SOLUTION_HPP
+
+#include "humans.hpp"
+
+#include <type_traits>
+
+// Primary Template
+template<typename Human>
+struct is_human : public std::false_type
+{
+};
+
+// Specialization for `Adult`
+template<>
+struct is_human<Adult> : public std::true_type
+{
+};
+
+// Specialization for `Child`
+template<>
+struct is_human<Child> : public std::true_type
+{
+};
+
+// Specialization for `Baby`
+template<>
+struct is_human<Baby> : public std::true_type
+{
+};
+
+#endif
