9d428bcf093bfd97865d1b7967118f527595be47
Mattan <mattanharnoy@gmail.com>
2025-08-07 16:31:37 +0300

    Complete solution for stage 3

--- Stages/3/solution/solution.hpp
+++ Stages/3/solution/solution.hpp
@@ -2,6 +2,7 @@
 #define SOLUTION_HPP
 
 #include "humans.hpp"
+#include "buffer.hpp"
 
 #include <type_traits>
 
@@ -31,5 +32,19 @@ struct is_human<Baby> : public std::true_type
 
 template<typename Human>
 constexpr bool is_human_v = is_human<Human>::value;
+
+// Primary Template
+// The deafult "false" case
+template<typename>
+struct is_buffer : public std::false_type
+{
+};
+
+// Partial Specialization
+// The "true" case in which a type is an instantiation of Buffer.
+template<typename T, int MemberCount>
+struct is_buffer<Buffer<T, MemberCount>> : public std::true_type
+{
+};
 
 #endif
